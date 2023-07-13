7f5e602336b45fc577fe8788594ee5976c2c8ffa
hichemf <hichem.faraoun@inttic.dz>
2023-07-13 14:50:03 +0100

    new modif

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          {/* Learn React */}
-          Hello W
+          Learn React
+          {/* Hello World */}
         </a>
       </header>
     </div>
