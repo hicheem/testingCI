c759c145a20f1211cc289dff69e96f6f90dd301d
hicheem <hichem.faraoun@inttic.dz>
2023-07-24 11:44:50 +0100

    app js modif

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-          Hello World
+          {/* Hello World */}
         </a>
       </header>
     </div>
